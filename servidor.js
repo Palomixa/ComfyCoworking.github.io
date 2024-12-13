cddb172b502daffbf3a5f4feed3a77af1fcedacd
Palomixa <palomixa_90@hotmail.com>
2024-12-13 11:27:04 +0100

    Cambios front y back

--- servidor.js
+++ servidor.js
@@ -35,6 +35,9 @@ app.use(reservasRoutes);
 app.use(consultarReservaRoutes);
 app.use(modificarReservaRoutes);
 app.use(eliminarReservaRoutes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "Indez", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 console.log("Servidor escuchando en el puerto", port);
